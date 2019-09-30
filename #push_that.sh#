c850f0ca44c1ba1022cfb5fd94bd76a73de9d014
splitsushi <splitsushi@users.noreply.github.com>
2019-09-30 18:18:58 +0200

    task06.0

---
+++ #push_that.sh#
@@ -0,0 +1,2 @@
+git add .;echo"commit repo";git commit -m $1;git push git@git.epitech.eu:/etienne.leroy@epitech.eu/$2 --force
+
--- prepare_my_repo.sh
+++ prepare_my_repo.sh
@@ -1,1 +1,2 @@
-blih -u etienne.leroy@epitech.eu repository create $1; blih -u etienne.le@epitech.eu repository setacl $1 ramassage-tek r
+blih -u etienne.leroy@epitech.eu repository create $1; blih -u etienne.le@epitech.eu repository setacl $1 ramassage-tek r;blih -u etienne.leroy@epitech.eu repository getacl $1
+
